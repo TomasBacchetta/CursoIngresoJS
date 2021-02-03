086c55077eed660f3c1aad19bdc7d15207adddd8
Tomas Bacchetta <faron6606@gmail.com>
2021-02-03 13:47:14 -0300

    resuelto

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,74 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
 function sumar()
 {	
-	alert("ok");	
+	let numero1;
+	let numero2;	
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numero1 + numero2;
+
+	alert(`El resultado de la suma es: ${resultado}`);
+
+	document.getElementById("txtIdNumeroUno") = "";
+	document.getElementById("txtIdNumeroDos") = "";
+
+
 }
 
 function restar()
 {
-	
+	let numero1;
+	let numero2;	
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numero1 - numero2;
+
+	alert(`El resultado de la resta es: ${resultado}`);
+
+	document.getElementById("txtIdNumeroUno") = "";
+	document.getElementById("txtIdNumeroDos") = "";
 }
 
 function multiplicar()
 { 
-	
+	let numero1;
+	let numero2;	
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numero1 * numero2;
+
+	alert(`El resultado de la multiplicación es: ${resultado}`);
+
+	document.getElementById("txtIdNumeroUno") = "";
+	document.getElementById("txtIdNumeroDos") = "";
 }
 
 function dividir()
 {
-	
+	let numero1;
+	let numero2;	
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numero1 / numero2;
+
+	alert(`El resultado de la división es: ${resultado}`);
+
+	document.getElementById("txtIdNumeroUno") = "";
+	document.getElementById("txtIdNumeroDos") = "";
 }
 
