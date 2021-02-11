ab87695dfd50bcbcec05cb9194b95da587d3bcd1
Tomas Bacchetta <faron6606@gmail.com>
2021-02-11 19:42:10 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -15,25 +15,13 @@ function mostrar()
 	
 	switch(mes){
 		case 'Julio':
-			alert("Abrigate que hace frio");
-			break;
-		
 		case 'Agosto':
 			alert("Abrigate que hace frio");
 			break;
 		
 		case 'Septiembre':
-			alert("Ya pasamos el frio, ahora calor!!!");
-			break;
-		
 		case 'Octubre':
-			alert("Ya pasamos el frio, ahora calor!!!");
-			break;
-			
 		case 'Noviembre':
-			alert("Ya pasamos el frio, ahora calor!!!");
-			break;
-		
 		case 'Diciembre':
 			alert("Ya pasamos el frio, ahora calor!!!");
 			break;
