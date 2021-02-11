bf3bddc827cc2bb60a2d7cd3239705ceae358bd1
Tomas Bacchetta <faron6606@gmail.com>
2021-02-11 19:05:19 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,35 @@
+/*
+Bacchetta, Tomás
+al seleccionar un mes informar.
+si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiesta!!!."
+*/
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
+	mes = document.getElementById('txtIdMes').value;
+	
+	switch (mes){
+		case 'Enero':
+			alert("¡Que comiences bien el año!");
+			break;
+
+		case 'Marzo':
+			alert("¡A clases!");
+			break;
+
+		case 'Julio':
+			alert("¡Se vienen las vacaciones!");
+			break;
+
+		case 'Diciembre':
+			alert("¡Felices fiestas!");
+			break;
+	}
 
 
 
-}//FIN DE LA FUNCIÓN
+}
