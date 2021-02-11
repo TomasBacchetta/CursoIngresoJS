4854725f9374090800cc7d88190b2997ec93cd3a
Tomas Bacchetta <faron6606@gmail.com>
2021-02-11 19:34:16 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,46 @@
+/*
+Bacchetta, Tomás
+al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.
+*/
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
+	
+	mes = document.getElementById('txtIdMes').value;
+	
+	switch(mes){
+		case 'Julio':
+			alert("Abrigate que hace frio");
+			break;
+		
+		case 'Agosto':
+			alert("Abrigate que hace frio");
+			break;
+		
+		case 'Septiembre':
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		
+		case 'Octubre':
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+			
+		case 'Noviembre':
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		
+		case 'Diciembre':
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
 
-
-
-
-}//FIN DE LA FUNCIÓN
+		default:
+			alert("Falta para el invierno");
+		break;
+		}
+	
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,44 @@
+/*
+Bacchetta, Tomás
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.
+*/
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
+	let dias;
+
+	mes = document.getElementById('txtIdMes').value;
+
+	switch (mes){
+		case 'Febrero':
+			dias = 28;
+			break;
+		
+		case 'Abril':
+			dias = 30;
+			break;
+
+		case 'Junio':
+			dias = 30;
+			break;
+
+		case 'Septiembre':
+			dias = 30;
+			break;
+
+		case 'Noviembre':
+			dias = 30;
+			break;
+
+		default:
+			dias = 31;
+			break;
+	}
 	
+	alert("Este mes tiene " + dias + " días.");
 	
-
-
-
-}//FIN DE LA FUNCIÓN
+}
