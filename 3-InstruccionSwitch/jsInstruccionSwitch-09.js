064c4ca3aa47a11bf43d72e6ec09f36aa837f1b8
Tomas Bacchetta <faron6606@gmail.com>
2021-02-11 21:00:18 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,73 @@
+/*
+Bacchetta, Tomás
+Ejercicio 9
+*/
+
+
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	const PRECIOBASE = 15000;
+	let estacion;
+	let destino;
+	let porcentajeAumento = 0;
+	let porcentajeDescuento = 0;
+	let precioNeto;
 
-}//FIN DE LA FUNCIÓN
+	estacion = document.getElementById('txtIdEstacion').value;
+
+	destino = document.getElementById('txtIdDestino').value;
+
+	switch (estacion){
+		case 'Invierno':
+			switch (destino){
+				case 'Mar del plata':
+					porcentajeDescuento = 20;
+					break;
+
+				case 'Bariloche':
+					porcentajeAumento = 20;
+					break;
+
+				default:
+					porcentajeDescuento = 10;
+					break;
+				
+			}
+			break;
+		
+		case 'Verano':
+			switch (destino){
+				case 'Bariloche':
+					porcentajeDescuento = 20;
+					break;
+
+				case 'Mar del plata':
+					porcentajeAumento = 20;
+					break;
+					
+				default:
+					porcentajeAumento = 10;
+					break;
+			
+			}
+			break;
+
+		default:
+			switch (destino){
+				case 'Bariloche':
+				case 'Mar del plata':
+				case 'Cataratas':
+				porcentajeAumento = 10;
+				break;
+
+			}
+			break;
+	
+	}
+
+	precioNeto = PRECIOBASE + (porcentajeAumento * PRECIOBASE / 100) - (porcentajeDescuento * PRECIOBASE / 100);
+	
+
+	alert("Para ir a " + destino + " en " + estacion + " deberá abonar " + precioNeto + " pesos.");
+
+}
