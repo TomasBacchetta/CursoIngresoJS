98ea82d2e6222d98acfeb2761641784db37592c8
Tomas Bacchetta <faron6606@gmail.com>
2021-02-11 21:08:01 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,21 @@
+/*
+Bacchetta, Tomás
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+en Otoño: Se viaja a todos los destinos.
+primavera: solo no se viaja a Bariloche
+*/
+
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion;
+	let destino;
 
-}//FIN DE LA FUNCIÓN
+	estacion = document.getElementById('txtIdEstacion').value;
+	destino = document.getElementById('txtIdDestino').value;
+
+	switch (estacion){
+		case 'Invierno':
+			
+	}
+}
