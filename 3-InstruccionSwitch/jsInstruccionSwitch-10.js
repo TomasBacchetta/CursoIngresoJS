df843e99c28c6f95185a280776ad24d88b8af3aa
Tomas Bacchetta <faron6606@gmail.com>
2021-02-11 21:15:52 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -16,6 +16,47 @@ function mostrar()
 
 	switch (estacion){
 		case 'Invierno':
-			
+			switch (destino){
+				case 'Bariloche':
+					alert("Se viaja");
+					break;
+				
+				default:
+					alert("No se viaja");
+					break;
+			}
+			break;
+		
+		case 'Verano':
+			switch (destino){
+				case 'Mar del plata':
+				case 'Cataratas':
+					alert ("Se viaja");
+					break;
+				
+				default:
+					alert ("No se viaja");
+					break;
+			}
+			break;
+
+		case 'Otoño':
+			alert ("Se viaja");
+			break;
+
+		default:
+			switch (destino){
+				case 'Bariloche':
+					alert ("No se viaja");
+					break;
+
+				default:
+					alert("Se viaja");
+					break;
+
+			}
+			break;
+	
 	}
+
 }
