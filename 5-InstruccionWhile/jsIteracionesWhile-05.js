47801fb4c403f08472b910a8685a6cb74d9f8946
Tomas Bacchetta <faron6606@gmail.com>
2021-02-12 20:59:23 -0300

    Resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,12 +1,31 @@
 /*
-Al presionar el botón pedir un sexo
-'f' para femenino, 'm' para masculino.*/
+Bacchetta, Tomás
+Al presionar el botón pedir un sexo 'f' para femenino, 'm' para masculino.
+ 
+*/ 
+
 function mostrar()
 {
 	var sexoIngresado;
+	var sexo;
+	
 	sexoIngresado = prompt("ingrese f ó m .");
 
+	while (sexoIngresado != 'm' && sexoIngresado != 'f'){
+		sexoIngresado = prompt("Dato erróneo. Ingrese f ó m");	
+	
+	}
 
+	switch (sexoIngresado){
+		case 'm':
+			sexo = 'Masculino';
+			break;
 
-	txtIdSexo.value=sexoIngresado;
-}//FIN DE LA FUNCIÓN
+		case 'f':
+			sexo = 'Femenino';
+			break;
+
+	}
+
+	document.getElementById('txtIdSexo').value = sexo;
+}
--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,36 @@
+/*   
+Bacchetta, Tomás
+Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.
+
+
+*/
+
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let contador;
+	let acumulador;
+	let numeroIngresado;
+	let promedio;
 
 	contador=0;
 	acumulador=0;
+
+	while (contador < 5){
+		contador = contador + 1;
+
+		numeroIngresado = prompt("Ingrese el número #" + contador);
+		numeroIngresado = parseInt(numeroIngresado);
+
+		acumulador = acumulador + numeroIngresado;
+		
+		console.log(acumulador);
 	
+	}
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+	promedio = acumulador / contador;
+	
+	document.getElementById('txtIdSuma').value = "La suma de los " + contador + " números es: " + acumulador;
+	
+	document.getElementById('txtIdPromedio').value = "El promedio de los " + contador + " números es: " + promedio;
+
+}
