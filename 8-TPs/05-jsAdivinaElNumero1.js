1c188a5753ea78064645d8c835ac6f64ec374b25
Tomas Bacchetta <faron6606@gmail.com>
2021-02-15 02:40:09 -0300

    Resuelto

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -6,21 +6,55 @@ secreto del 1 al 100, en la pantalla del juego
   por terminado el juego con un mensaje similar a este: 
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
+
+txtIdIntentos
+
 */
-var numeroSecreto; 
-var contadorIntentos;
+let numeroSecreto; 
+let contadorIntentos;
+
+contadorIntentos = 0;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+	numeroSecreto = Math.floor(Math.random() * 100 ) + 1;
+  numeroSecreto = parseInt(numeroSecreto);
 
 }
 
 function verificar()
 {
-	
-	
+	let numeroIngresado;
+
+  numeroIngresado = document.getElementById('txtIdNumero').value;
+  numeroIngresado = parseInt(numeroIngresado);
+  
+
+  while (((isNaN(numeroIngresado) == true) || numeroIngresado < 1 || numeroIngresado >100) ){ 
+    numeroIngresado = prompt("Tiene que ingresar un número, y que sea entre 1 y 100!");
+    numeroIngresado = parseInt(numeroIngresado);
+    document.getElementById('txtIdNumero').value = numeroIngresado;
+    
+  }
+  
+  
+  if (numeroIngresado == numeroSecreto){
+    alert("Usted acertó en " + contadorIntentos + " intentos!!");
+    contadorIntentos = 0;
+  
+  } else {
+    if (numeroIngresado > numeroSecreto){
+      alert("Se pasó.");
+      contadorIntentos = contadorIntentos + 1;
+    
+    } else {
+      alert("Le falta.");
+      contadorIntentos = contadorIntentos + 1;
+
+    }
+
+  }  
+
+  document.getElementById('txtIdIntentos').value = contadorIntentos;;
+
 }
